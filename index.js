84a120261e2b7a338be4902b894567f8a2729502
Patrick Lebsack <p_lebbysack@hotmail.com>
2019-02-12 22:25:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var recipes = {};
 
-function updateO
+function updateObjectWithKeyAndValue(recipes, key, value){
+  
+}
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
   recipes[key]=value
