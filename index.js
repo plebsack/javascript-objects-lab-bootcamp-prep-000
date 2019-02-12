01f9dfe5aa68b1abbadacac825a1caffdae35333
Patrick Lebsack <p_lebbysack@hotmail.com>
2019-02-12 22:44:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObject = Object.assign({}, object)
+  var newObject = Object.assign({}, object, { [key]: value })r
   delete newObject.object
 }
 
